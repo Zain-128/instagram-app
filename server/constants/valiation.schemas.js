49a72e55c7e54eec77fcbc6e7d7e4ec1865f0c97
Zain-128 <100517579+Zain-128@users.noreply.github.com>
2024-08-18 11:54:21 -0700

    register User controller

---
+++ server/constants/valiation.schemas.js
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+// Define the Joi schema
+const userSchema = Joi.object({
+  username: Joi.string().required().max(255).messages({
+    "string.empty": "Username is required",
+    "string.max": "Username must be less than 256 characters",
+  }),
+  email: Joi.string().email().required().max(255).messages({
+    "string.empty": "Email is required",
+    "string.email": "Email must be a valid email address",
+    "string.max": "Email must be less than 256 characters",
+  }),
+  password: Joi.string().required().min(6).messages({
+    "string.empty": "Password is required",
+    "string.min": "Password must be at least 6 characters",
+  }),
+  profilePic: Joi.string().uri().allow(""),
+  bio: Joi.string().allow(""),
+  followers: Joi.array().items(Joi.string().hex().length(24)),
+  following: Joi.array().items(Joi.string().hex().length(24)),
+  posts: Joi.array().items(Joi.string().hex().length(24)),
+  bookmarks: Joi.array().items(Joi.string().hex().length(24)),
+  gender: Joi.string().valid("male", "female", "other").optional(),
+});
+
+export { userSchema };
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,41 @@
-export const registerUser = async () => {
+import UserModel from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { userSchema } from "../constants/valiation.schemas.js";
+
+export const registerUser = async (req, res, next) => {
   try {
-  } catch (erro) {}
+    const { error } = userSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    const { email, password, username } = req.body;
+    const isUserExists = await UserModel.findOne({
+      $or: [{ email }, { username }],
+    });
+    if (!!isUserExists) {
+      return next({
+        status: 409,
+        message: "User Already Exists ! ",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    let user = await UserModel.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    user = new USERDTO(user);
+
+    return res.status(200).json({
+      message: "User Created Successfully !",
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
